b30c1a5ef681ef2e10f0a0dfaba2549f64306647
Abdur Rahman <146614165+AbdurRahman4991@users.noreply.github.com>
2025-10-08 09:20:26 +0600

    Update row

--- lib/main.dart
+++ lib/main.dart
@@ -32,22 +32,24 @@ class Calculator extends StatelessWidget{
           Expanded(
             flex: 1,
             child: 
-                  Row(children: <Widget> [Container(
+                  Row(children: <Widget> [Expanded(child: Container(
           child: Text('1', style: TextStyle(fontSize: 36,          
            color: Colors.white),),
            color: Colors.green,                
-        ),],
+        ), ),],
         ),
           ),
 
         Expanded(
           flex: 1,
           child: 
-        Row(children: <Widget>[Container(
+        Row(children: <Widget>[Expanded(child: Container(
           child: Text('2', style: TextStyle(fontSize: 36,
             color: Colors.white ),),
             color: Colors.red,
-        ),],
+        ),),],
+
+        
         ),
         ),
 
@@ -61,20 +63,20 @@ class Calculator extends StatelessWidget{
         ),),
 
        Expanded(child:
-        Row(children: <Widget>[Container(
+        Row(children: <Widget>[Expanded(child:Container(
           child: Text('4', style: TextStyle(fontSize: 36,
           color: Colors.white
           ),),
           color: Colors.blue,
-        ),],
+        ),)],
         ),),
 
         Expanded(child: 
-        Row(children: [Container(
+        Row(children: <Widget>[Expanded(child: Container(
           child: Text('5', style: TextStyle(fontSize: 36,
           color: Colors.white),),
           color: Colors.yellow,
-        ),],
+        ), ),],
         
         ),
         ),
